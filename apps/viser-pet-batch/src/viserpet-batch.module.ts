af674a4f09d148d943a45b2ea89a586a69df6aa5
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-11-20 05:30:48 -0800

    feat: configure env variable

--- apps/viser-pet-batch/src/viserpet-batch.module.ts
+++ apps/viser-pet-batch/src/viserpet-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ViserpetBatchController } from './viserpet-batch.controller';
 import { ViserpetBatchService } from './viserpet-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [ViserpetBatchController],
   providers: [ViserpetBatchService],
 })
