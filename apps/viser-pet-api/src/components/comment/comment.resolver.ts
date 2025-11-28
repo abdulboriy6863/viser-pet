2adb992ac42c920df69045a3a1cb801892be14e0
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-11-28 11:12:56 -0800

    feat: develop updateComment, getComments and removeCommentByAdmin GraphQL API busness logic

--- apps/viser-pet-api/src/components/comment/comment.resolver.ts
+++ apps/viser-pet-api/src/components/comment/comment.resolver.ts
@@ -1,11 +1,17 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../guards/auth.guard';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
 import { AuthMember } from '../decorators/authMember.decorator';
 import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { ObjectId } from 'mongoose';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { WithoutGuard } from '../guards/without.guard';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { Roles } from '../decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../guards/roles.guard';
 
 @Resolver()
 export class CommentResolver {
@@ -19,5 +25,36 @@ export class CommentResolver {
 	): Promise<Comment> {
 		console.log('Mutation: createComment');
 		return await this.commentService.createComment(memberId, input);
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => Comment)
+	public async updateComment(
+		@Args('input') input: CommentUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comment> {
+		console.log('Mutation: updateComment');
+		return await this.commentService.updateComment(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(() => Comments)
+	public async getComments(
+		@Args('input') input: CommentsInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comments> {
+		console.log('Mutation: updateComment');
+		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
+		return await this.commentService.getComments(memberId, input);
+	}
+
+	/* ADMIN */
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Comment)
+	public async removeCommentByAdmin(@Args('commentId') input: string): Promise<Comment> {
+		console.log('Mutation: removeCommentByAdmin');
+		const commentId = shapeIntoMongoObjectId(input);
+		return await this.commentService.removeCommentByAdmin(commentId);
 	}
 }
--- apps/viser-pet-api/src/components/comment/comment.service.ts
+++ apps/viser-pet-api/src/components/comment/comment.service.ts
@@ -5,9 +5,12 @@ import { MemberService } from '../member/member.service';
 import { ProductService } from '../product/product.service';
 import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { BlogPostService } from '../blog-post/blog-post.service';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
-import { Message } from '../../libs/enums/common.enum';
-import { CommentGroup } from '../../libs/enums/comment.enum';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { T } from '../../libs/types/common';
+import { lookupMember } from '../../libs/config';
 
 @Injectable()
 export class CommentService {
@@ -54,6 +57,57 @@ export class CommentService {
 		}
 
 		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+		return result;
+	}
+
+	public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
+		const { _id } = input;
+		const result = await this.commentModel.findOneAndUpdate(
+			{
+				_id: _id,
+				memberId: memberId,
+				commentStatus: CommentStatus.ACTIVE,
+			},
+			input,
+			{
+				new: true,
+			},
+		);
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+		return result;
+	}
+
+	public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+		const { commentRefId } = input.search;
+		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+
+		const result: Comments[] = await this.commentModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [
+							{ $skip: (input.page - 1) * input.limit },
+							{ $limit: input.limit },
+							//likedbyme
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
+	}
+
+	public async removeCommentByAdmin(input: ObjectId): Promise<Comment> {
+		const result = await this.commentModel.findByIdAndDelete(input);
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
 		return result;
 	}
 }
