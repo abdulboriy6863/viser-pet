07701fa7b21f0a84e26bbb1463614855d9c6a886
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-12-06 03:14:05 -0800

    feat: create nestar batch

---
+++ apps/viser-pet-batch/src/lib/config.ts
@@ -0,0 +1,7 @@
+/**********************************************
+ *               BATCH CONSTANTA              *
+ **********************************************/
+
+export const BATCH_ROLLERBACK = 'BATCH_ROLLERBACK';
+export const BATCH_TOP_PROPERTIES = ' BATCH_TOP_PROPERTIES';
+export const BATCH_TOP_AGENTS = 'BATCH_TOP_AGENTS';
--- apps/viser-pet-batch/src/viserpet-batch.module.ts
+++ apps/viser-pet-batch/src/viserpet-batch.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ViserpetBatchController } from './viserpet-batch.controller';
 import { ViserpetBatchService } from './viserpet-batch.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [ViserpetBatchController],
   providers: [ViserpetBatchService],
 })
